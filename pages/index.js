8c6ea4daa67b56f559c0dfc6ee4b05e67f9f152e
Benjamin Russell <benjaminrobertrussell@gmail.com>
2021-11-30 13:47:10 -0500

    connected app to chatengine.io

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,34 @@ import axios from 'axios';
 
 export default function Auth() {
     const {
+        username,
         setUsername,
+        secret,
         setSecret,
     } = useContext(Context);
+
+    const router = useRouter();
+
+    // handles signing in or signing up for using "let's Chat" app.
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        // validate username and secret.
+        if (username.length < 1 || secret.length < 1) return;
+
+        // submit info to chatengine.io
+        axios.put(
+            {username, secret},
+            {headers: {"Private-key": `${process.env.REACT_APP_CHATENGINE_API_KEY}`}}
+        )
+        .then(res => router.push('/chats'))
+
+    }
+
     return (
         <div className='background'>
             <div className='auth-container'>
-                <form className='auth-form' onSubmit={(e) => e.preventDefault()}>
+                <form className='auth-form' onSubmit={(e) => onSubmit(e)}>
                     <div className='auth-title'>Lets Chat</div>
 
                     <div className='input-container'>
